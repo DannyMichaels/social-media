6c5b4d085a03b48dfa00cfcddf9391b8cc51bd9e
Daniel Michael <itzdanielmichael@gmail.com>
2020-10-26 23:08:12 -0400

    using jwt and creating middleware to verify token

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,19 @@
 const { Router } = require("express");
-// const controllers = require('../controllers/somethinghere')
-
 const router = Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../keys')
+const requireLogin = require('../middleware/requireLogin')
+
+router.get('/protected', requireLogin,(req, res) => {
+   try {
+       res.send("hello user")
+    } catch (error) {
+      res.status(500).json({error: error.message})
+     }
+})
 
 router.post("/signup", (req, res) => {
   try {
@@ -52,7 +61,9 @@ router.post("/signin", (req, res) => {
     bcrypt.compare(password, savedUser.password)
       .then(doMatch => {
         if (doMatch) {
-        res.json({message: "successfully signed in"})
+        // res.json({message: "successfully signed in"})
+          const token = jwt.sign({ id: savedUser._id }, JWT_SECRET)
+          res.json({token})
         }
         else {
           return res.status(422).json({ error: "Invalid email or password" });
